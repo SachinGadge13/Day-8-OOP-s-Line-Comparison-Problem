c3eb023b3d4d2602e86aaea65038e8b2f3f50b3a
SachinGadge13 <gadges14@gmail.com>
2022-10-11 19:12:44 +0530

    Added:Welcome Message

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+This is Line Comparison Computation Program..
---
+++ src/com/bridgelabz/lineComparison/LineComparison.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.lineComparison;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program.");
+
+	}
+
+}
